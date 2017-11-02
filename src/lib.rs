5169069f20fdc16f656cd4026d2d89a896eba78c
M Farkas-Dyck <strake888@gmail.com>
2017-11-01 22:54:36 -0800

    try_clone_from

--- src/lib.rs
+++ src/lib.rs
@@ -7,9 +7,19 @@ use void::Void;
 pub trait TryClone: Sized {
     type Error;
     fn try_clone(&self) -> Result<Self, Self::Error>;
+    fn try_clone_from(&mut self, other: &Self) -> Result<(), Self::Error> {
+        other.try_clone().map(|new| *self = new)
+    }
 }
 
 impl<T: Clone> TryClone for T {
     type Error = Void;
+
+    #[inline(always)]
     fn try_clone(&self) -> Result<Self, Void> { Ok(self.clone()) }
+
+    #[inline(always)]
+    fn try_clone_from(&mut self, other: &Self) -> Result<(), Void> {
+        Ok(self.clone_from(other))
+    }
 }
