f0c8c8ec6b43c48a9a3f4b24990a10d904926e28
M Farkas-Dyck <strake888@gmail.com>
2017-12-15 02:18:34 -0800

    rename as fallible; TryFrom

--- src/lib.rs
+++ src/lib.rs
@@ -23,3 +23,15 @@ impl<T: Clone> TryClone for T {
         Ok(self.clone_from(other))
     }
 }
+
+pub trait TryFrom<A>: Sized {
+    type Error;
+    fn try_from(A) -> Result<Self, Self::Error>;
+}
+
+impl<A, T: From<A>> TryFrom<A> for T {
+    type Error = Void;
+
+    #[inline(always)]
+    fn try_from(a: A) -> Result<Self, Self::Error> { Ok(Self::from(a)) }
+}
